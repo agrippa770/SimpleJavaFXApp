45aa8f2d9e9bf0415c1af4c7d5269eb9991558df
Agrippa770 <Agrippa770@gmail.com>
2026-09-24 13:41:13 +0200

    First JavaFX Lab complete

---
+++ src/main/java/com/example/hellofx/HelloJavaFX.java
@@ -0,0 +1,45 @@
+package com.example.hellofx;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class HelloJavaFX extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        Label message = new Label("Welcome, Agrippa Moyo!");
+
+        Button button = new Button("Start");
+
+        Button resetButton = new Button("Reset");
+
+        button.setOnAction(event ->
+                message.setText("Great! You clicked the button.")
+        );
+        resetButton.setOnAction(event ->
+                message.setText("Welcome, Agrippa Moyo")
+        );
+
+        VBox layout = new VBox(20);
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(message, button, resetButton);
+
+        Scene scene = new Scene(layout, 500, 300);
+
+        stage.setTitle("My First JavaFX Application - 202505368");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+
